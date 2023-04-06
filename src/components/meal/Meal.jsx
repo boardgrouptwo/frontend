0df92c17166d5bfc17dcf8bf0d246cab3f02b6c8
Soyeonthdus <dorthy311@gmail.com>
2023-04-06 18:21:35 +0900

    qna작업중

--- src/components/meal/Meal.jsx
+++ src/components/meal/Meal.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Calender from './Calender';
 import MainHeader from '../include/MainHeader';
-import MainBottom from '../include/MainBottom'
+import Noticebar from '../board/notice/Noticebar';
 
 const Meal = () => {
   return (
     <>
       <MainHeader />
+      <Noticebar/>
       <Calender />
     </>
   );
