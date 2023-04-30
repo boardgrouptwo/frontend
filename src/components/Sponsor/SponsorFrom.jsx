7b2ff53de5d87f282728f3c0af3c010023ef7709
welsper53 <welsper53@gmail.com>
2023-04-30 22:56:56 +0900

    23.04.30 결제로직 추가 중..

--- src/components/Sponsor/SponsorFrom.jsx
+++ src/components/Sponsor/SponsorFrom.jsx
@@ -56,6 +56,49 @@ const SponsorFrom = () => {
       }
     },[]);
 
+///////////////////////////////////////// 아임포트
+  var IMP = window.IMP; // 생략가능
+  console.log("IMP : " + IMP);
+  //가맹점 식별코드
+  IMP.init("imp17705726"); 
+
+const requestPay = () => {
+  console.log("호출")
+  let msg;
+
+  IMP.request_pay({ // param
+    //pg: "kakaopay.TC0ONETIME",    // 상점 ID
+    pg: "html5_inicis",
+    pay_method: "card",
+    merchant_uid: "ORD20180131-0000011",
+    name: "노르웨이 회전 의자",//결제창에서 보여질 이름
+    amount: 64900,//실제 결제되는 가격
+    buyer_email: "gildong@gmail.com",
+    buyer_name: "홍길동",
+    buyer_tel: "010-4242-4242",
+    buyer_addr: "서울특별시 강남구 신사동",
+    buyer_postcode: "01181"
+  }, rsp => { // callback
+    console.log(rsp)
+    if (rsp.success) {
+      msg = '결제가 완료되었습니다.';
+      msg += '고유ID : ' + rsp.imp_uid;
+      msg += '상점 거래ID : ' + rsp.merchant_uid;
+      msg += '결제 금액 : ' + rsp.paid_amount;
+      msg += '카드 승인번호 : ' + rsp.apply_num;
+    } else {
+      msg = '결제에 실패하였습니다.';
+      msg += '에러내용 : ' + rsp.error_msg;
+    }
+    
+    alert(msg);
+  });
+}
+
+
+///////////////////////////////////////// 아임포트
+
+
 // 폼 제출 이벤트 처리
     const handleSubmit = async(event) => {   // form 컴포넌트에서 submit 할 때 실행됨
       const form = event.currentTarget;
